1a589fb9dcd38fd1d198fc52755e3c6aa7dd26ac
VaiYav <yakovlev.valentin97@gmail.com>
2017-09-07 13:33:46 +0300

    add fade slider in sectionInfo

--- src/components/section/sectionInfo.vue
+++ src/components/section/sectionInfo.vue
@@ -1,9 +1,9 @@
 <template>
   <section id="sectionInfo" class="sectionInfo light_theme">
-    <div class="container">
+    <div class="container" >
   <swiper :options="swiperOptionTop" class="gallery-top" :only-fade="false" ref="swiperTop">
-    <swiper-slide v-for="item in mainInfo.items" :label="item.link">
-      <section class="hero is-primary hero-body box-item">
+    <swiper-slide v-for="item in mainInfo.items" :label="item.link" >
+      <section class="hero is-primary hero-body box-item" >
         <div class="level">
           <div class="level-left">
             <h2 class="title">{{ item.title }}</h2>
@@ -24,6 +24,8 @@
 </template>
 
 <script>
+  /* eslint-disable no-trailing-spaces */
+
   import { mapGetters } from 'vuex'
   import { Tabs, TabPane } from 'vue-bulma-tabs'
   require('swiper/dist/css/swiper.css')
@@ -45,8 +47,12 @@
           notNextTick: true,
           nextButton: '.swiper-button-next',
           prevButton: '.swiper-button-prev',
-          spaceBetween: 10,
-          direction: 'vertical'
+          spaceBetween: 50,
+          direction: 'horizontal',
+          effect: 'fade',
+          touchMoveStopPropagation: true,
+          hoverStopPropagation: true,
+          autoplay: 2500
         },
         swiperOptionThumbs: {
           spaceBetween: 10,
@@ -61,6 +67,15 @@
       const swiperThumbs = this.$refs.swiperThumbs.swiper
       swiperTop.params.control = swiperThumbs
       swiperThumbs.params.control = swiperTop
+      swiperThumbs.container[0].onclick = function () {
+        swiperTop.stopAutoplay()
+      }
+      swiperTop.container[0].onmouseover = function () {
+        swiperTop.stopAutoplay()
+      }
+      swiperTop.container[0].onmouseout = function () {
+        swiperTop.startAutoplay()
+      }
     },
     computed: {
       ...mapGetters({
@@ -73,7 +88,6 @@
   @import '../../scss/variables';
   .sectionInfo{
     padding:2rem 0;
-    min-height:500px;
     .level{
       align-items: flex-start;
     }
@@ -89,16 +103,125 @@
       }
     }
     .swiper-container{
-      height:500px;
+      @include responsive(maxTablet){
+        padding: 0 0 25px 0;
+      }
+    }
+    & > .container{
+      display: flex;
+      flex-direction: row-reverse;
+      @include responsive(maxTablet){
+        flex-direction: column-reverse;
+      }
+    }
+    .hero.is-primary{
+      padding: 15px 20px;
+      @include responsive(maxTablet){
+        padding: 1rem;
+      }
+    }
+  }
+  .gallery-top{
+    background: #fff;
+    border-radius: 10px;
+    .swiper-slide{
+      opacity: 0 !important;
+      .is-primary{
+box-shadow: none;
+        border:none;
+      }
+    }
+    .swiper-slide-active{
+      opacity: 1 !important;
     }
   }
 .gallery-thumbs{
-  .swiper-wrapper{
-    transform: none !important;
+  width: 200px !important;
+    @include responsive(maxTablet){
+      height: inherit !important;
+      width:100% !important;
+      text-align: center;
 
   }
+  .swiper-wrapper{
+    transform: none !important;
+    display: flex;
+    flex-direction: column;
+    @include responsive(maxTablet){
+      flex-direction: row;
+      justify-content: center;
+      flex-wrap: wrap;
+
+    }
+  }
   .swiper-slide{
-    width:200px !important;
+    max-width:165px !important;
+    min-height:60px;
+    height: initial;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin:0 !important;
+    span{
+      white-space: normal;
+      word-break: break-word;
+      padding: 10px 15px;
+      min-width:120px;
+      max-width: 120px;
+      text-transform: uppercase;
+      border-radius: 5px;
+      font-weight: 500;
+      background-color: #fff;
+      font-size: 14px;
+      line-height: 1.4;
+      margin: 0;
+      font-weight: bold;
+      position: relative;
+      z-index: 2;
+      border: 1px solid #ededed;
+      cursor: pointer;
+      @include responsive(maxTablet){
+        height: 70%;
+        display: flex;
+        align-items: center;
+        line-height: 1.2;
+      }
+      @media screen and (max-width:555px){
+        padding: 5px;
+        font-size:12px;
+        text-align: center;
+        justify-content: center;
+      }
+    }
+  }
+  .swiper-slide-active{
+    span{
+      background: $color14;
+      color: $color0;
+      position: relative;
+      &:before{
+        display: block;
+        content: '';
+        top:40%;
+        right: -5px;
+        position: absolute;
+        width: 0;
+        height: 0;
+        border-top: 5px solid transparent;
+        border-left: 5px solid $color14;
+        border-bottom: 5px solid transparent;
+        @include responsive(maxTablet) {
+          bottom: -5px;
+          top:initial;
+          left: calc(50% - 5px);
+          border-left: 5px solid transparent;
+          border-right: 5px solid transparent;
+          border-top: 5px solid $color14;
+          border-bottom:initial;
+        }
+      }
+    }
+
   }
 }
   .level{
@@ -130,102 +253,9 @@
     background: $color13;
     padding: 2rem 0;
   }
-  .tabs{
-    margin: 0 25px 0 0;
-    @include responsive(maxTablet){
-      margin: 0;
-    }
-  }
-  .tabs a{
+  .swiper-container {
     width: 100%;
-    height: 100%;
-    @include responsive(maxPhone){
-      padding: 2px;
-      font-size: 14px;
-      line-height: 1.3;
-
-    }
+    height: 100% !important;
   }
-  .tabs li.is-active a{
-    border-bottom-color: #667eea;
-    color: $color1;
-  }
-  .tab-content{
-    margin: 0 !important;
-  }
-  .vue-bulma-tabs.is-layout-top{
-    flex-direction: row !important;
-    @include responsive(maxTablet){
-      flex-direction: column !important;
-
-    }
-  }
-  .tabs ul{
-    border: none;
-    align-items: flex-end;
-    @include responsive(maxTablet){
-      flex-direction: row;
-      overflow: hidden;
-      width: 100%;
-      justify-content: space-between;
-    }
-    li{
-      margin: 0 0 10px 0;
-      padding: 0 20px;
-      overflow: hidden;
-      max-width: 235px;
-      text-align: center;
-      font-weight: bold;
-      width:100%;
-      @include responsive(maxTablet){
-        width: 33%;
-        height: 90%;
-        padding: 7px 1px;
-      }
-      span{
-        white-space: normal;
-        word-break: break-word;
-      }
-    }
-  }
-  .tabs li.is-active a{
-    background: $color14;
-    color: $color0;
-  }
-  .tab-list{
-    flex-direction: column;
-  }
-  .tabs a{
-    border:none;
-    background: $color0;
-    border-radius: 5px;
-    @include responsive(maxTablet){
-      width: 100%;
-    }
-  }
-  .tabs li.is-active a{
-    position: relative;
-    &:before{
-      display: block;
-      content: '';
-      top:40%;
-      right: -5px;
-      position: absolute;
-      width: 0;
-      height: 0;
-      border-top: 5px solid transparent;
-      border-left: 5px solid $color14;
-      border-bottom: 5px solid transparent;
-      @include responsive(maxTablet) {
-        bottom: -5px;
-        top:initial;
-        left: calc(50% - 5px);
-        border-left: 5px solid transparent;
-        border-right: 5px solid transparent;
-        border-top: 5px solid $color14;
-        border-bottom:initial;
-      }
-      }
-    }
 
 </style>
